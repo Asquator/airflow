9cdc04a8b2489e18b3ef001808938317d86845c2
Asquator <vistalus@protonmail.com>
2025-09-24 00:17:30 +0300

    Added max_active_tasks

--- files/dags/control_dag.py
+++ files/dags/control_dag.py
@@ -2,15 +2,18 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
+N=4000
+
 with DAG(
     dag_id="simple_bash_mapped_dag",
     start_date=datetime(2025, 1, 1),
     schedule=None,
+    max_active_tasks=N,
     catchup=False,
 ) as dag:
     BashOperator.partial(
         task_id="echo_task",
         do_xcom_push=False
     ).expand(
-        bash_command=['echo 0'] * 4000
+        bash_command=['echo 0'] * N
     )
